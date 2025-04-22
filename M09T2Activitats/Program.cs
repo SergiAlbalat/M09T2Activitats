7beb464e5c0ccc95e1967f0927c596200dd27f4e
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-22 13:39:57 +0200

    Add project files.

---
+++ M09T2Activitats/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace M09T2Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using(StreamWriter writer = new StreamWriter("processes.txt"))
+            {
+                Process[] progresses = Process.GetProcesses();
+                foreach (Process process in progresses)
+                {
+                    Console.WriteLine(process.Id);
+                    writer.WriteLine(process.Id);
+                }
+            }
+        }
+    }
+}
