2859a053eaaffca06abef95eaf285fbd9d1d7739
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-22 13:54:16 +0200

    Exercici 3 acabagat Closes #3

--- M09T2Activitats/Program.cs
+++ M09T2Activitats/Program.cs
@@ -6,13 +6,31 @@ namespace M09T2Activitats
     {
         public static void Main()
         {
-            using(StreamWriter writer = new StreamWriter("processes.txt"))
+            using (StreamWriter writer = new StreamWriter("processes.txt"))
             {
                 Process[] progresses = Process.GetProcesses();
                 foreach (Process process in progresses)
                 {
                     Console.WriteLine(process.Id);
-                    writer.WriteLine(process.Id); 
+                    writer.WriteLine(process.Id);
+                }
+            }
+            PrintThreads();
+        }
+
+        public static void PrintThreads()
+        {
+            Process[] procesos = Process.GetProcessesByName("msedge");
+            if(procesos.Length == 0)
+            {
+                Console.WriteLine("There is no processes in edge");
+                return;
+            }
+            foreach (Process process in procesos)
+            {
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    Console.WriteLine($"Thread ID: {thread.Id}, Start Time: {thread.StartTime}, Priority: {thread.PriorityLevel}");
                 }
             }
         }
